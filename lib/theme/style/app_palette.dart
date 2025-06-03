ddb8334e1acc16b761807cb80f027cb7ea9d4b0f
Abdulmajid Usman <abstractmajid@gmail.com>
2025-06-03 18:39:18 +0100

    add app_palette

---
+++ lib/theme/style/app_palette.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class AppPalette {
+  static const white = Color(0XFFFFFFFF);
+  static const black = Color(0XFF000000);
+  static const catSkillWhite = Color(0XFFE2E8F0);
+  static const athensGray = Color(0XFFF6F5F8);
+  static const malibu = Color(0XFF60B5FF);
+  static const pickledBluewood = Color(0XFF334155);
+  static const mountainMeadow = Color(0XFF10B981);
+  static const royalBlue = Color(0XFF2563EB);
+  static const shipGray = Color(0XFF49454F);
+  static const dustyGray = Color(0XFF999999);
+  static const alabaster = Color(0XFFFBFBFB);
+  static const slateGray = Color(0XFF64748B);
+  static const radicalRed = Color(0XFFFF2D55);
+  static const oxfordBlue = Color(0XFF3C4856);
+}
