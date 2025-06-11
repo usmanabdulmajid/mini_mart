b9857270c0d6b388f9d68251b86285ee55fc0320
Abdulmajid Usman <abstractmajid@gmail.com>
2025-06-11 20:11:17 +0100

    remove debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Chatie',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
