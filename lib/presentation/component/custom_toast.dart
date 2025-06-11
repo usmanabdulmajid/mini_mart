feaeccbc2eed9dd5b52045c2c5b68e12fc875ee9
Abdulmajid Usman <abstractmajid@gmail.com>
2025-06-11 20:05:42 +0100

    add top-down animation to custom-toast

--- lib/presentation/component/custom_toast.dart
+++ lib/presentation/component/custom_toast.dart
@@ -54,33 +54,123 @@ class CustomToast extends StatelessWidget {
     BuildContext context, {
     required String message,
     Duration duration = const Duration(seconds: 3),
+    Duration animationDuration = const Duration(milliseconds: 300),
   }) {
-    final overlay = Overlay.of(context);
+    final overlay = Overlay.of(context, rootOverlay: true);
     late OverlayEntry overlayEntry;
 
     overlayEntry = OverlayEntry(
       builder:
-          (context) => Positioned(
-            top: MediaQuery.of(context).padding.top + 20,
-            left: 0,
-            right: 0,
-            child: Material(
-              color: Colors.transparent,
-              child: CustomToast(
-                message: message,
-                duration: duration,
-                onClose: () => overlayEntry.remove(),
-              ),
-            ),
+          (context) => _AnimatedToastWrapper(
+            message: message,
+            duration: duration,
+            animationDuration: animationDuration,
+            onClose: () => overlayEntry.remove(),
           ),
     );
 
     overlay.insert(overlayEntry);
 
-    Future.delayed(duration, () {
+    Future.delayed(duration + animationDuration, () {
       if (overlayEntry.mounted) {
         overlayEntry.remove();
       }
     });
+  }
+}
+
+class _AnimatedToastWrapper extends StatefulWidget {
+  final String message;
+  final Duration duration;
+  final Duration animationDuration;
+  final VoidCallback onClose;
+
+  const _AnimatedToastWrapper({
+    required this.message,
+    required this.duration,
+    required this.animationDuration,
+    required this.onClose,
+  });
+
+  @override
+  State<_AnimatedToastWrapper> createState() => _AnimatedToastWrapperState();
+}
+
+class _AnimatedToastWrapperState extends State<_AnimatedToastWrapper>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+  bool _isClosing = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: widget.animationDuration,
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0.0, -1.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
+    Future.delayed(widget.duration, () {
+      if (mounted && !_isClosing) {
+        _dismissToast();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _dismissToast() {
+    if (_isClosing) return;
+
+    setState(() {
+      _isClosing = true;
+    });
+
+    _controller.reverse().then((_) {
+      if (mounted) {
+        widget.onClose();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).padding.top + 20,
+      left: 0,
+      right: 0,
+      child: SlideTransition(
+        position: _slideAnimation,
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: Material(
+            color: Colors.transparent,
+            child: CustomToast(
+              message: widget.message,
+              duration: widget.duration,
+              onClose: _dismissToast,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
